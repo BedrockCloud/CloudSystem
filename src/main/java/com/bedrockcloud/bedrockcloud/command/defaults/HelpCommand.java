e8c3469e4e4db9c8c98e94ca9349d7b655416696
VipexDev <a.app@gmx.net>
2022-09-03 18:09:42 +0200

    Update HelpCommand.java
    Eventuell kann man das ja in so ner form adden

--- src/main/java/com/bedrockcloud/bedrockcloud/command/defaults/HelpCommand.java
+++ src/main/java/com/bedrockcloud/bedrockcloud/command/defaults/HelpCommand.java
@@ -11,5 +11,6 @@ public class HelpCommand extends Command implements Loggable
     
     @Override
     protected void onCommand(final String[] args) {
+     this.getLogger().info("§a--Commands-- §r\n\n§7- end §eStops the Cloud§r\n§7- help §eShows you all commands§r\n§7- info §eShows you informations about the System§r\n§7- player §eShows infos about Players§r\n§7- server §eManage your Servers§r\n§7- software §eUpdate Versions§r\n§7- template §eManage your Templates§r");
     }
 }

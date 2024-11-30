882f18e4a66d704edd5ed23be6d7be2f75f8a314
xxFLORII <teammeloneyt@gmail.com>
2024-11-30 07:58:50 +0100

    bruh

--- src/bedrockcloud/util/VersionInfo.php
+++ src/bedrockcloud/util/VersionInfo.php
@@ -4,7 +4,7 @@ namespace bedrockcloud\util;
 
 final class VersionInfo {
 
-    public const VERSION = "2.0.3";
+    public const VERSION = "2.0.4";
     public const DEVELOPERS = ["r3pt1s", "xxFLORII"];
     public const BETA = false;
 }
